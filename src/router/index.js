4c8b3e44a91b14f0ea8fc2887058b3fd603fba6a
Allan Chain <allanchain@pku.edu.cn>
2020-03-20 11:48:05 +0800

    name route chunks

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '@/views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -16,38 +15,35 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    component: () => import('@/views/Login')
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "login" */ '@/views/Login')
   },
   {
     path: '/bookB116',
     name: 'bookingIndex',
-    component: () => import('@/views/BookingIndex')
+    component: () =>
+      import(/* webpackChunkName: "bookingIndex" */ '@/views/BookingIndex')
   },
   {
     path: '/bookB116/book',
     name: 'bookingMain',
-    component: () => import('@/views/BookingMain')
+    component: () =>
+      import(/* webpackChunkName: "bookingMain" */ '@/views/BookingMain')
   },
   {
     path: '/html/:src',
     name: 'markdownView',
     props: true,
-    component: () => import('@/views/MarkdownView')
+    component: () =>
+      import(/* webpackChunkName: "markdown" */ '@/views/MarkdownView')
   },
   {
     path: '*',
     name: 'error',
-    component: () => import('@/views/Error')
+    component: () => import(/* webpackChunkName: "error" */'@/views/Error')
   }
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 ]
 
 const router = new VueRouter({
